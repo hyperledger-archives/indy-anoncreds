8ab48fb999e997b87e6daa17de3f24acfee0382b
Aditya Singh <aditya@evernym.us>
2016-06-22 15:28:25 +0530

    Revert "Remove the changes for predicate logic, Make the Issuer class configurable in order to mock it"
    This reverts commit 2f90e6f09e7992bbeecc185ced25dd25fb29e114.

---
+++ anoncreds/test/test_predicate_credential.py
@@ -0,0 +1,62 @@
+import pytest
+from anoncreds.test.helper import getProver, getPresentationToken
+from anoncreds.protocol.issuer import Issuer
+from anoncreds.protocol.verifier import Verifier
+from anoncreds.protocol.prover import fourSquares
+from functools import reduce
+
+
+@pytest.fixture(scope="module")
+def attrNames():
+    return 'name', 'age', 'sex'
+
+
+@pytest.fixture(scope="module")
+def issuerPk(issuer):
+    # Return issuer's public key
+    return {"gvt": issuer.PK}
+
+
+@pytest.fixture(scope="module")
+def proverAndAttrs1(issuerPk):
+    attrs = {'name': 'Aditya Pratap Singh', 'age': 25, 'sex': 'male'}
+    return getProver(attrs, issuerPk)
+
+
+@pytest.fixture(scope="module")
+def issuer(attrNames):
+    # Create issuer
+    return Issuer(attrNames)
+
+
+@pytest.fixture(scope="module")
+def verifier(issuerPk):
+    # Setup verifier
+    return Verifier(issuerPk)
+
+def testPredicateCredentials(issuer, proverAndAttrs1, verifier):
+    prover, encodedAttrs, attrs = proverAndAttrs1
+    encodedAttrsDict = {"gvt": encodedAttrs}
+
+    presentationToken = getPresentationToken({"gvt": issuer}, prover, encodedAttrsDict)
+
+    nonce = verifier.Nonce
+
+    revealedAttrs = ['name']
+    predicate = {'age': 18}
+    proof = prover.preparePredicateProof(credential=presentationToken, attrs=encodedAttrs,
+                                         revealedAttrs=revealedAttrs, nonce=nonce,
+                                         predicate=predicate, encodedAttrsDict=encodedAttrsDict)
+
+    verify_status = verifier.verify_proof(proof=proof, nonce=nonce,
+                                          attrs=encodedAttrs, revealedAttrs=revealedAttrs,
+                                          predicate=predicate, encodedAttrsDict=encodedAttrsDict)
+
+    assert verify_status
+
+
+def testQuadEquationLagranges():
+    delta = 85
+    u1, u2, u3, u4 = tuple(fourSquares(delta))
+    print("u1: {0} u2: {1} u3: {2} u4: {3}".format(u1, u2, u3, u4))
+    assert (u1 ** 2) + (u2 ** 2) + (u3 ** 2) + (u4 ** 2) == delta

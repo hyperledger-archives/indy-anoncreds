db208efa6bd81847d357f33535865d0c55a6aa6d
Aditya Singh <aditya@evernym.us>
2016-06-22 15:29:49 +0530

    Revert "Add deleted methods and test file for predicate logic"
    This reverts commit f2c9658517bfebc5f9ee26c0e88da220447fa7b5.

--- anoncreds/protocol/prover.py
+++ anoncreds/protocol/prover.py
@@ -1,10 +1,7 @@
 from charm.core.math.integer import randomBits, integer
-from math import sqrt, floor
-from functools import reduce
 from typing import Dict
 
-from anoncreds.protocol.globals import lvprime, lmvect, lestart, letilde, \
-    lvtilde, lms, lutilde, lrtilde, lalphatilde
+from anoncreds.protocol.globals import lvprime, lmvect, lestart, letilde, lvtilde, lms
 from anoncreds.protocol.models import Credential, IssuerPublicKey
 from anoncreds.protocol.utils import get_hash, get_values_of_dicts, \
     splitRevealedAttributes
@@ -104,153 +101,11 @@ class Prover:
 
         return c, evect, vvect, mvect, Aprime
 
-    def preparePredicateProof(self, credential, attrs, revealedAttrs, nonce,
-                              predicate, encodedAttrsDict):
-
-        TauList = []
-        CList = []
-        C = {}
-        T = {}
-        Aprime = {}
-        vprime = {}
-        eprime = {}
-        etilde = {}
-        vtilde = {}
-        evect = {}
-        vvect = {}
-        uvect = {}
-        u = {}
-        utilde = {}
-        r = {}
-        rtilde = {}
-        rvect = {}
-        alphatilde = 0
-        alphavect = 0
-        iterations = 4
-
-        Ar, Aur = splitRevealedAttributes(attrs, revealedAttrs)
-
-        mtilde = {}
-        for key, value in Aur.items():
-            mtilde[key] = integer(randomBits(lmvect))
-        mtilde["0"] = integer(randomBits(lmvect))
-
-        for key, val in credential.items():
-            Ra = integer(randomBits(lvprime))
-            A, e, v = val
-            N, R, S, Z = self.pk_i[key]
-            includedAttrs = encodedAttrsDict[key]
-
-            Aprime[key] = A * (S ** Ra) % N
-            vprime[key] = (v - e * Ra)
-            eprime[key] = e - (2 ** lestart)
-
-            etilde[key] = integer(randomBits(letilde))
-            vtilde[key] = integer(randomBits(lvtilde))
-
-            Rur = 1 % N
-
-            for k, value in Aur.items():
-                if k in includedAttrs:
-                    Rur = Rur * (R[k] ** mtilde[k])
-            Rur *= R["0"] ** mtilde["0"]
-
-            T[key] = ((Aprime[key] ** etilde[key]) * Rur * (S ** vtilde[key])) % N
-
-            TauList.append(T[key])
-            CList.append(Aprime[key])
-            C["Aprime"] = Aprime[key]
-
-        for key, val in predicate.items():
-            #TODO: Remove the hardcoded value for 'gvt'
-            N, R, S, Z = self.pk_i['gvt']
-
-            delta = self.m[key] - predicate[key]
-            u = fourSquares(delta)
-
-            for i in range(0, iterations):
-                r[str(i)] = integer(randomBits(lvprime))
-            r["delta"] = integer(randomBits(lvprime))
-
-            Tval = {}
-            for i in range(0, iterations):
-                Tval[str(i)] = (Z ** u[i]) * (S ** r[str(i)]) % N
-                utilde[str(i)] = integer(randomBits(lutilde))
-                rtilde[str(i)] = integer(randomBits(lrtilde))
-            Tval["delta"] = (Z ** delta) * (S ** r["delta"]) % N
-            rtilde["delta"] = integer(randomBits(lrtilde))
-
-            CList.extend(get_values_of_dicts(Tval))
-            #TODO: Remove the hardcoded value for 'Tval'
-            C['Tval'] = Tval
-
-            Tbar = {}
-            for i in range(0, iterations):
-                Tbar[str(i)] = (Z ** utilde[str(i)]) * (S ** rtilde[str(i)]) % N
-            Tbar["delta"] = (Z ** Aur[key]) * (S ** rtilde["delta"]) % N
-
-            TauList.extend(get_values_of_dicts(Tbar))
-
-            alphatilde = integer(randomBits(lalphatilde))
-
-            Q = 1 % N
-            for i in range(0, iterations):
-                Q *= Tval[str(i)] ** utilde[str(i)]
-            Q *= S ** alphatilde
-
-            TauList.append(Q)
-
-        c = integer(get_hash(*reduce(lambda x, y: x+y, [TauList, CList, [nonce]])))
-
-        for key, val in credential.items():
-            evect[key] = etilde[key] + (c * eprime[key])
-            vvect[key] = vtilde[key] + (c * vprime[key])
-
-        mvect = {}
-        for k, value in Aur.items():
-            mvect[str(k)] = mtilde[str(k)] + (c * attrs[str(k)])
-        mvect["0"] = mtilde["0"] + (c * self._ms)
-
-        subProofC = {"evect": evect, "vvect": vvect, "mvect": mvect, "Aprime": Aprime}
-
-        for key, val in predicate.items():
-            urproduct = 0
-            for i in range(0, iterations):
-                uvect[str(i)] = utilde[str(i)] + c * u[i]
-                rvect[str(i)] = rtilde[str(i)] + c * r[str(i)]
-                urproduct += u[i] * r[str(i)]
-            rvect["delta"] = rtilde["delta"] + c * r["delta"]
-
-            alphavect = alphatilde + c * (r["delta"] - urproduct)
-
-        subProofPredicate = {"uvect": uvect, "rvect": rvect, "mvect": mvect, "alphavect": alphavect}
-
-        return c, subProofC, subProofPredicate, C, CList
-
-
     @property
     def U(self):
         return self._U
 
-
     @property
     def vprime(self):
         return self._vprime
-
-
-def findLargestSquareLessThan(x):
-    sqrtx = floor(sqrt(x))
-    return sqrtx
-
-
-def fourSquares(delta):
-    u1 = findLargestSquareLessThan(delta)
-    u2 = findLargestSquareLessThan(delta - (u1 ** 2))
-    u3 = findLargestSquareLessThan(delta - (u1 ** 2) - (u2 ** 2))
-    u4 = findLargestSquareLessThan(delta - (u1 ** 2) - (u2 ** 2) - (u3 ** 2))
-    if (u1 ** 2) + (u2 ** 2) + (u3 ** 2) + (u4 ** 2) == delta:
-        return list((u1, u2, u3, u4))
-    else:
-        raise Exception("Cannot get the four squares for delta {0}".format(delta))
-
 

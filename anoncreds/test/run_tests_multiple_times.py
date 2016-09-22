198fdee8f58d9280c26677bf32403712892396f1
Jason Law <jason@evernym.us>
2016-09-22 08:45:50 -0600

    running test multiple times

--- anoncreds/test/run_tests_multiple_times.py
+++ anoncreds/test/run_tests_multiple_times.py
@@ -2,11 +2,14 @@ import pytest
 
 
 def run():
-    for x in range(100):
+    for x in range(1000):
         itr = x + 1
         print('running {}'.format(itr))
-        pytest.main('-x')
-        # pytest.main('-x test_greater_eq_predicate.py::testPredicateMultipleIssuers')
+        # pytest.main('-x')
+        tst = 'test_greater_eq_predicate.py::testPredicateGreaterEqMultiIssuers'
+        result = pytest.main('-x {}'.format(tst))
+        if result > 0:
+            break
 
 if __name__ == '__main__':
     run()

ef480000baebe95aaa53908207ffbaa11612bdf2
Victor Muzychenko <mzkvct@gmail.com>
2017-02-08 17:11:51 +0300

    use 'schema' instead of 'claim def' in test_single_prover_single_issuer

--- anoncreds/test/test_single_prover_single_issuer.py
+++ anoncreds/test/test_single_prover_single_issuer.py
@@ -9,40 +9,40 @@ from anoncreds.test.conftest import presentProofAndVerify
 async def testPrimaryClaimOnlyEmpty(prover1, verifier, claimsProver1Gvt, nonce):
     proofInput = ProofInput([])
     claims, revealedAttrs = await prover1._findClaims(proofInput)
-    claims = {claimDefKey: ProofClaims(
+    claims = {schemaKey: ProofClaims(
         Claims(primaryClaim=proofClaim.claims.primaryClaim))
-              for claimDefKey, proofClaim in claims.items()}
+              for schemaKey, proofClaim in claims.items()}
     proof = await prover1._prepareProof(claims, nonce)
     assert await verifier.verify(proofInput, proof, revealedAttrs, nonce)
 
 
 @pytest.mark.asyncio
 async def testPrimaryClaimNoPredicates(prover1, verifier, claimsProver1Gvt,
-                                       nonce, claimDefGvtId,
+                                       nonce, schemaGvtId,
                                        attrRepo):
     revealledAttrs = ['name']
     proofInput = ProofInput(revealledAttrs)
     claims, revealedAttrs = await prover1._findClaims(proofInput)
     claims = {
-        claimDefKey: ProofClaims(
+        schemaKey: ProofClaims(
             Claims(primaryClaim=proofClaim.claims.primaryClaim),
             revealedAttrs=revealledAttrs)
-        for claimDefKey, proofClaim in claims.items()}
+        for schemaKey, proofClaim in claims.items()}
     proof = await prover1._prepareProof(claims, nonce)
     assert await verifier.verify(proofInput, proof, revealedAttrs, nonce)
 
 
 @pytest.mark.asyncio
 async def testPrimaryClaimPredicatesOnly(prover1, verifier, claimsProver1Gvt,
-                                         nonce, claimDefGvtId,
+                                         nonce, schemaGvtId,
                                          attrRepo):
     predicates = [PredicateGE('age', 18)]
     proofInput = ProofInput(predicates=predicates)
     claims, revealedAttrs = await prover1._findClaims(proofInput)
-    claims = {claimDefKey: ProofClaims(
+    claims = {schemaKey: ProofClaims(
         Claims(primaryClaim=proofClaim.claims.primaryClaim),
         predicates=predicates)
-              for claimDefKey, proofClaim in claims.items()}
+              for schemaKey, proofClaim in claims.items()}
     proof = await prover1._prepareProof(claims, nonce)
     assert await verifier.verify(proofInput, proof, revealedAttrs, nonce)
 
@@ -120,27 +120,27 @@ def testAttrsInClaims(claimsProver1Gvt, attrsProver1Gvt):
 
 
 @pytest.mark.asyncio
-async def testUParamShouldBeSame(prover1, verifier, issuerGvt, claimDefGvtId,
+async def testUParamShouldBeSame(prover1, verifier, issuerGvt, schemaGvtId,
                                  attrsProver1Gvt, keysGvt,
                                  issueAccumulatorGvt):
-    claimsReq = await prover1.createClaimRequest(claimDefGvtId)
+    claimsReq = await prover1.createClaimRequest(schemaGvtId)
 
     claimsReq = claimsReq._replace(U=claimsReq.U ** 2)
-    claims = await issuerGvt.issueClaim(claimDefGvtId, claimsReq)
-    await prover1.processClaim(claimDefGvtId, claims)
+    claims = await issuerGvt.issueClaim(schemaGvtId, claimsReq)
+    await prover1.processClaim(schemaGvtId, claims)
 
     proofInput = ProofInput(['name'], [])
     assert not await presentProofAndVerify(verifier, proofInput, prover1)
 
 
 @pytest.mark.asyncio
-async def testUrParamShouldBeSame(prover1, issuerGvt, claimDefGvtId,
+async def testUrParamShouldBeSame(prover1, issuerGvt, schemaGvtId,
                                   attrsProver1Gvt, keysGvt,
                                   issueAccumulatorGvt):
-    claimsReq = await prover1.createClaimRequest(claimDefGvtId)
+    claimsReq = await prover1.createClaimRequest(schemaGvtId)
 
     claimsReq = claimsReq._replace(Ur=claimsReq.Ur ** 2)
-    claims = await issuerGvt.issueClaim(claimDefGvtId, claimsReq)
+    claims = await issuerGvt.issueClaim(schemaGvtId, claimsReq)
 
     with pytest.raises(ValueError):
-        await prover1.processClaim(claimDefGvtId, claims)
+        await prover1.processClaim(schemaGvtId, claims)

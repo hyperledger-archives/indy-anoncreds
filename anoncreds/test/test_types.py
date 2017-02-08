a226aa3fa6afdc48ded642beccd5bb5f121eb5b2
Victor Muzychenko <mzkvct@gmail.com>
2017-02-08 17:14:00 +0300

    use 'schema' instead of 'claim def' in test_types

--- anoncreds/test/test_types.py
+++ anoncreds/test/test_types.py
@@ -7,19 +7,19 @@ from anoncreds.protocol.utils import toDictWithStrValues, fromDictWithStrValues
 from config.config import cmod
 
 
-def testClaimDefKeyFromToDict():
-    claimDefKey = SchemaKey(name='claimDefName', version='1.0',
+def testSchemaKeyFromToDict():
+    schemaKey = SchemaKey(name='claimDefName', version='1.0',
                             issuerId='issuer1')
-    assert claimDefKey == SchemaKey.fromStrDict(
-        claimDefKey.toStrDict())
+    assert schemaKey == SchemaKey.fromStrDict(
+        schemaKey.toStrDict())
 
 
-def testClaimDefFromToDict():
-    claimDef = Schema(name='claimDefName', version='1.0',
-                      claimDefType='CL',
+def testSchemaFromToDict():
+    schema = Schema(name='claimDefName', version='1.0',
+                      schemaType='CL',
                       attrNames=['attr1', 'attr2', 'attr3'],
                       issuerId='issuer1')
-    assert claimDef == Schema.fromStrDict(claimDef.toStrDict())
+    assert schema == Schema.fromStrDict(schema.toStrDict())
 
 
 def testPKFromToDict():
